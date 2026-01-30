d7349c1059c50e4ca29229639296c3b57910cce5
Vikram Rakate <vikramrakate1972@gmail.com>
2026-01-30 18:59:42 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.29%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1;
+
+        long long exp = n;   
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        return fastPow(x, exp);
+    }
+
+    double fastPow(double x, long long n) {
+        if (n == 0) return 1;
+
+        double half = fastPow(x, n / 2);
+
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return half * half * x;
+    }
+};
